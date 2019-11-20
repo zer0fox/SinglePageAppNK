6fc44176c283292f92e93e045a098b6ec4b3a8ac
Dionysis Marinos <zer0fox@hotmail.com>
2019-11-20 23:03:20 +0200

    less template created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {  
     res.render('index')
