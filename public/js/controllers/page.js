a146d77f281bd44f439080600c6300f27a69703a
Dionysis Marinos <zer0fox@hotmail.com>
2019-11-21 09:57:50 +0200

    moved around stuff to the correct folders

--- public/js/controllers/page.js
+++ public/js/controllers/page.js
@@ -1,6 +1,6 @@
-const ViewModel = function(first, last) {
+const PageViewModel = function(first, last) {
     this.pageTitle = ko.observable("ECG basics");
     this.lastName = ko.observable(last);
 };
 
-ko.applyBindings(new ViewModel());
+ko.applyBindings(new PageViewModel());
