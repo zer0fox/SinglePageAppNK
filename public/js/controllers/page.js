7eac0045b29d76585d75e2652ddeaa1684bbca6e
Dionysis Marinos <zer0fox@hotmail.com>
2019-11-22 20:59:27 +0200

    added memory anchors

--- public/js/controllers/page.js
+++ public/js/controllers/page.js
@@ -1,6 +1,18 @@
-const PageViewModel = function(model) {
+const PageViewModel = function (model) {
     this.pageTitle = ko.observable(model.pageTitle);
-    //this.lastName = ko.observable(last);
+    this.breadcrumb = ko.observable(model.breadcrumb);
+    this.upNextList = ko.observable(model.upNextList);
+    this.memoryAnchorsList = ko.observable(model.memoryAnchorsList);
+
+    this.getSourceIcon = (source) => {
+        let icon = "";
+        switch (source) {
+            case "Youtube": icon = "youtube.png"; break;
+            case "Picmonic": icon = "ologo.png"; break;
+            case "Osmosis": icon = "plogo.png"; break;
+        }
+        return "images/"+icon;
+    };
 };
 
 ko.applyBindings(new PageViewModel(new PageModel()));
--- public/js/models/page.js
+++ public/js/models/page.js
@@ -1,3 +1,17 @@
-const PageModel = function(first, last) {
+const PageModel = function() {
     this.pageTitle = "ECG basics";
+    this.breadcrumb = [
+        {url: "#", name: "Cardiovascular"},
+        {url: "#", name: "Physiology: Cardiovascular Physiology"},
+        {url: "#", name: "Electrocardiography"}
+    ];
+    this.upNextList = [
+        {url: "#", img: "next1.png", title: "Blood pressure, blood flow and resistance", source: "Osmosis"}
+    ];
+    this.memoryAnchorsList = [
+        {url: "#", img: "ma1.png", title: "Electrocardiography Handwritten", source: "Youtube"},
+        {url: "#", img: "ma2.png", title: "Electrocardiography Khan", source: "Youtube"},
+        {url: "#", img: "ma3.jpg", title: "12 Lead Contiguous Leads - Lateral Wall MI", source: "Picmonic"},
+        {url: "#", img: "ma4.jpg", title: "12 Lead Contiguous Leads - Lateral Wall MI", source: "Picmonic"}
+    ];
 };
